e4eece3002638edf564cc3dc01dbce1ab7ea5921
lomakim <lomakim@gmail.com>
2026-03-01 13:26:01 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 // COMSC-210 | Lab 14 | Loma Kim
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+const int WIDTH = 8;
 
 class Color{
     private:
@@ -21,12 +24,33 @@ class Color{
 };
 
 int main() {
+    //DECLARATIONS
+    Color c1, c2, c3, c4, c5;
+    c1.setRed(1);
+    c1.setGreen(1);
+    c1.setBlue(1);
 
+    c2.setRed(1);
+    c2.setGreen(1);
+    c2.setBlue(1);
 
+    c3.setRed(1);
+    c3.setGreen(1);
+    c3.setBlue(1);
+
+    c4.setRed(1);
+    c4.setGreen(1);
+    c4.setBlue(1);
+
+    c5.setRed(1);
+    c5.setGreen(1);
+    c5.setBlue(1);
 
     return 0;
 }
 
-void Color::print(){
-    cout << 
+void Color::print() const {
+    cout << setw(WIDTH) << "Red: " << red;
+    cout << setw(WIDTH) << "Green: " << green;
+    cout << setw(WIDTH) << "Blue: " << blue << endl;
 }
